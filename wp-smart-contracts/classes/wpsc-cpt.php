2cba81f0b9f46383e4dd2bbef5ad25e65a518edd
duncan4123 <54425677+duncan4123@users.noreply.github.com>
2021-06-13 15:01:57 +1000

    init

---
+++ wp-smart-contracts/classes/wpsc-cpt.php
@@ -0,0 +1,25 @@
+<?php
+
+if( ! defined( 'ABSPATH' ) ) die;
+
+/**
+ * Coins CPT
+ */
+require_once("wpsc-cpt-coin.php");
+
+
+/**
+ * Crowdfunding CPT
+ */
+require_once("wpsc-cpt-crowdfunding.php");
+
+/**
+ * ICO CPT
+ */
+require_once("wpsc-cpt-ico.php");
+
+/**
+ * NFT CPT
+ */
+require_once("wpsc-cpt-nft-collection.php");
+require_once("wpsc-cpt-nft.php");
